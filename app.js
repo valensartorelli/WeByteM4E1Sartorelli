0c60c3e92421dc55053a8001814914cc050e52b1
valensartorelli <valen.sartorelli@gmail.com>
2021-04-03 01:18:15 -0300

    Actualizo archivos segun micro desafios

--- app.js
+++ app.js
@@ -8,6 +8,12 @@ app.use(express.static(publicPath));
 app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, '/views/home.html'));
 });
+app.get('/register.html', (req,res) => {
+    res.sendFile(path.join(__dirname, '/views/register.html'));
+});
+app.get('/login.html', (req,res) => {
+    res.sendFile(path.join(__dirname, '/views/login.html'));
+});
 
 app.listen(3000, () => {
     console.log('El servidor esta corriendo en el puerto 3000');
